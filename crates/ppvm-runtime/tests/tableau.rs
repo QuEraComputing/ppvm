d3c46b640253780cbb3e5d65ad78aa19a1b1551c
David Plankensteiner <dplankensteiner@quera.com>
2026-02-09 16:14:57 +0100

    More tests

--- crates/ppvm-runtime/tests/tableau.rs
+++ crates/ppvm-runtime/tests/tableau.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use num::complex::Complex64;
+use num::complex::{Complex, Complex64};
 use ppvm_runtime::{config::dashmap::ByteFxHashF64, prelude::*};
 
 #[test]
@@ -34,7 +34,38 @@ fn generalized_tableau() {
 
     tableau.t_adj(0);
 
-    println!("{}", tableau);
-
     assert_eq!(tableau.coefficients.len(), 1);
+
+    tableau.t(0);
+    tableau.t(1);
+
+    // NOTE: since IZ|psi> = (IZ) * ZZ |psi> = ZI|psi>, we don't branch again
+    assert_eq!(tableau.coefficients.len(), 2);
+
+    let mut sorted_coefficients = tableau.coefficients.clone();
+    sorted_coefficients.sort_by(|entry1, entry2| entry1.1.cmp(&entry2.1));
+
+    const COS_PI_OVER_8: f64 = 0.9238795325112867; // cos(pi/8)
+    const SIN_PI_OVER_8: f64 = 0.3826834323650898; // sin(pi/8)
+    let expected_coefficients = vec![
+        Complex {
+            re: COS_PI_OVER_8 * COS_PI_OVER_8 - SIN_PI_OVER_8 * SIN_PI_OVER_8,
+            im: 0.0,
+        },
+        Complex {
+            re: 0.0,
+            im: -2.0 * SIN_PI_OVER_8 * COS_PI_OVER_8,
+        },
+    ];
+
+    for ((val1, idx1), (idx2, val2)) in sorted_coefficients
+        .iter()
+        .zip(expected_coefficients.iter().enumerate())
+    {
+        assert_eq!(idx1, &idx2);
+        assert!((val1.re - val2.re).abs() < 1e-11);
+        assert!((val1.im - val2.im).abs() < 1e-11);
+    }
+
+    println!("{}", tableau);
 }

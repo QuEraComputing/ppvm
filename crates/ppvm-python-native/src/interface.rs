04e9d568e0a98ca78087346a1ade4b84e091c4cc
David Plankensteiner <david-pl@users.noreply.github.com>
2026-02-23 17:47:15 +0100

    Add python bindings (#20)
    * Draft implementation for python interface
    
    * Rename ppvm-py to ppvm-python
    
    * Use macro loop to create interfaces
    
    * Properly wrap things in a Python class
    
    * Properly separate rust backend and python wrapper
    
    * Remove triggers from CI to make sure we don't accidentally release something
    
    * Convenience method and make it a frozen dataclass
    
    * Add convenience method for error probs in two-qubit pauli error
    
    * Add pre-commit hooks
    
    * Remove pycache
    
    * Expose noise
    
    * Lower python version bound
    
    * Make passing in qubits optional and add pytest dep
    
    * Don't pass in unneeded arguments
    
    * Remove unused file
    
    * Add docstrings (thanks claude)
    
    * Implement copy and equality
    
    * Len and rename the terms parameter to initial_terms
    
    * Add accessor method to get out terms and coefficients
    
    * Rename python package to ppvm
    
    * Wrap weight calculation
    
    * Rename wrapper.py
    
    * Implement an overlap method for PauliSums
    
    * Add the overlap to the python interface
    
    * Document expressions for rotation gates
    
    * Better construction methods by parsing strings of different format
    
    * Also expose truncation kwargs in new method
    
    * Fix parsing error

---
+++ crates/ppvm-python-native/src/interface.rs
@@ -0,0 +1,220 @@
+use paste::paste;
+use ppvm_runtime::prelude::*;
+use ppvm_runtime::strategy::{CoefficientThreshold, CombinedStrategy, MaxPauliWeight};
+use pyo3::prelude::*;
+
+// adapted from https://pyo3.rs/v0.27.1/class.html#no-generic-parameters
+macro_rules! create_interface {
+    ($name: ident, $type: ident) => {
+        #[pyclass]
+        pub struct $name {
+            inner: PauliSum<$type>,
+        }
+        #[pymethods]
+        impl $name {
+            #[new]
+            #[pyo3(signature = (n_qubits, min_abs_coeff = 1e-10, max_pauli_weight = usize::MAX, terms = Vec::<String>::new(), coefficients = Vec::<f64>::new()))]
+            pub fn new(
+                n_qubits: usize,
+                min_abs_coeff: f64,
+                max_pauli_weight: usize,
+                terms: Vec<String>,
+                coefficients: Vec<f64>
+            ) -> Self {
+
+                // TODO: this is not ideal since we could skip one of the strategies completely; need to look into
+                // how we can do this in the macro here
+                let strategy = CombinedStrategy(CoefficientThreshold(min_abs_coeff), MaxPauliWeight(max_pauli_weight));
+                let mut ps = PauliSum::builder()
+                    .n_qubits(n_qubits)
+                    .strategy(strategy)
+                    .capacity(n_qubits)
+                    .build();
+
+                assert_eq!(
+                    terms.len(),
+                    coefficients.len(),
+                    "Initial terms and coefficients need to be of same length!"
+                );
+
+                for (term, c) in terms.iter().zip(coefficients.iter()) {
+                    ps += (term.to_owned(), c.to_owned());
+                }
+
+                Self { inner: ps }
+            }
+
+            fn __repr__(&self) -> String {
+                // TODO: expose some more details e.g. for debugging
+                format!("PauliSum({})", self.inner)
+            }
+
+            fn __str__(&self) -> String {
+                self.inner.to_string()
+            }
+
+            pub fn trace(&self, pattern: String) -> f64 {
+                let pat: PauliPattern = pattern.into();
+                let result = self.inner.trace(&pat);
+                result
+            }
+
+            pub fn overlap_with_zero(&self) -> f64 {
+                self.trace("Z?*".to_owned())
+            }
+
+            pub fn overlap(&self, other: &Self) -> f64 {
+                self.inner.overlap(&other.inner)
+            }
+
+            // NOTE: macros can't be used in pymethods block
+            // could either use multiple-pymethods feature (adds dependencies)
+            // or better yet create working impl for all strategies
+
+            // clifford
+            pub fn x(&mut self, addr0: usize) {
+                self.inner.x(addr0);
+                self.inner.truncate();
+            }
+
+            pub fn y(&mut self, addr0: usize) {
+                self.inner.y(addr0);
+                self.inner.truncate();
+            }
+
+            pub fn z(&mut self, addr0: usize) {
+                self.inner.z(addr0);
+                self.inner.truncate();
+            }
+
+            pub fn h(&mut self, addr0: usize) {
+                self.inner.h(addr0);
+                self.inner.truncate();
+            }
+
+            pub fn s(&mut self, addr0: usize) {
+                self.inner.s(addr0);
+                self.inner.truncate();
+            }
+
+            pub fn cnot(&mut self, addr0: usize, addr1: usize) {
+                self.inner.cnot(addr0, addr1);
+                self.inner.truncate();
+            }
+
+            pub fn cz(&mut self, addr0: usize, addr1: usize) {
+                self.inner.cz(addr0, addr1);
+                self.inner.truncate();
+            }
+
+            // rot1
+            pub fn rx(&mut self, addr0: usize, theta: f64) {
+                self.inner.rx(addr0, theta);
+                self.inner.truncate();
+            }
+
+            pub fn ry(&mut self, addr0: usize, theta: f64) {
+                self.inner.ry(addr0, theta);
+                self.inner.truncate();
+            }
+
+            pub fn rz(&mut self, addr0: usize, theta: f64) {
+                self.inner.rz(addr0, theta);
+                self.inner.truncate();
+            }
+
+            // rot2
+            pub fn rxx(&mut self, addr0: usize, addr1: usize, theta: f64) {
+                self.inner.rxx(addr0, addr1, theta);
+                self.inner.truncate();
+            }
+
+            pub fn ryy(&mut self, addr0: usize, addr1: usize, theta: f64) {
+                self.inner.ryy(addr0, addr1, theta);
+                self.inner.truncate();
+            }
+
+            pub fn rzz(&mut self, addr0: usize, addr1: usize, theta: f64) {
+                self.inner.rzz(addr0, addr1, theta);
+                self.inner.truncate();
+            }
+
+            // noise
+            pub fn pauli_error(&mut self, addr0: usize, p: [f64; 3]) {
+                self.inner.pauli_error(addr0, p);
+                self.inner.truncate();
+            }
+
+            pub fn two_qubit_pauli_error(&mut self, addr0: usize, addr1: usize, p: [f64; 15]) {
+                self.inner.two_qubit_pauli_error(addr0, addr1, p);
+                self.inner.truncate();
+            }
+
+
+            // some python niceties
+
+            fn __copy__(&self) -> Self {
+                Self { inner: self.inner.clone() }
+            }
+
+            fn __richcmp__(&self, other: PyRef<$name>, op: pyo3::basic::CompareOp) -> PyResult<bool> {
+                match op {
+                    pyo3::basic::CompareOp::Eq => Ok(self.inner == other.inner),
+                    pyo3::basic::CompareOp::Ne => Ok(self.inner != other.inner),
+                    _ => Err(pyo3::exceptions::PyNotImplementedError::new_err("Only equality and inequality comparisons are supported for PauliSum.")),
+                }
+            }
+
+            fn __deepcopy__(&self, _memo: &Bound<'_, PyAny>) -> Self {
+                Self { inner: self.inner.clone() }
+            }
+
+            fn __len__(&self) -> usize {
+                self.inner.len()
+            }
+
+            pub fn terms(&self) -> Vec<(String, f64)> {
+                self.inner.data().iter().map(|(k, v)| (k.to_string(), *v)).collect()
+            }
+
+            pub fn weights(&self) -> Vec<(String, usize)> {
+                self.inner.data().iter().map(|(k, _v)| (k.to_string(), k.weight())).collect()
+            }
+
+            pub fn current_max_weight(&self) -> usize {
+                self.inner.data().iter().map(|(k, _v)| k.weight()).max().unwrap_or(0)
+            }
+        }
+    };
+}
+
+macro_rules! create_interface_range {
+    ($name: ident, $( $n: expr),* ) => {
+        paste! {
+        $(
+            type [<$name$n>] = config::indexmap::ByteFxHashF64<{(2 as usize).pow($n)}, CombinedStrategy<CoefficientThreshold, MaxPauliWeight>>;
+            create_interface!([<PauliSum$name$n>], [<$name$n>]);
+        )*
+        }
+    };
+}
+
+create_interface_range!(
+    IndexMapFxHash,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15
+);

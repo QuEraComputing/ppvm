6a06cb39d4a17c539b08b0af4c02db833b5d9f04
David Plankensteiner <dplankensteiner@quera.com>
2026-03-12 11:09:15 +0100

    Add a benchmark for measurements

--- crates/ppvm-runtime/benches/tableau.rs
+++ crates/ppvm-runtime/benches/tableau.rs
@@ -11,7 +11,7 @@ pub fn benchmark_suite_tableau(c: &mut Criterion, name: impl AsRef<str>) {
 
         group.bench_function(format!("tableau-scaling-{}", n_qubits), |b| {
             b.iter_batched_ref(
-                || tableau.clone(),
+                || tableau.fork(None),
                 |tableau| {
                     tableau.h(0);
                     tableau.t(0);
@@ -28,6 +28,29 @@ pub fn benchmark_suite_tableau(c: &mut Criterion, name: impl AsRef<str>) {
                 criterion::BatchSize::SmallInput,
             );
         });
+
+        let mut tab =
+            GeneralizedTableau::<config::indexmap::ByteFxHashF64<8>, usize>::new(n_qubits, 1e-12);
+        tab.h(0);
+        tab.t(0);
+        for i in 0..n_qubits - 1 {
+            tab.cnot(i, i + 1);
+        }
+
+        // some more T gates
+        tab.t(n_qubits - 1);
+        tab.t(n_qubits - 2);
+        group.bench_function(format!("tableau-measure-scaling-{}", n_qubits), |b| {
+            b.iter_batched_ref(
+                || tab.fork(None),
+                |tab| {
+                    for i in 0..n_qubits {
+                        tab.measure(i);
+                    }
+                },
+                criterion::BatchSize::SmallInput,
+            );
+        });
     }
 
     let mut tableau =
@@ -39,7 +62,7 @@ pub fn benchmark_suite_tableau(c: &mut Criterion, name: impl AsRef<str>) {
     for tgate_num in 1..11 {
         group.bench_function(format!("tableau-t-gate-{}", tgate_num), |b| {
             b.iter_batched_ref(
-                || tableau.clone(),
+                || tableau.fork(None),
                 |tableau| {
                     for i in 0..tgate_num {
                         tableau.t(i);

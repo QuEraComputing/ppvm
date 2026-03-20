d17d72866b06ab2bd7bfe5bb42280c73ab19881a
David Plankensteiner <dplankensteiner@quera.com>
2026-03-20 16:34:33 -0400

    Implement a new storage type that uses u64 bitpacking

---
+++ crates/ppvm-runtime/src/config/fx64hash.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+use std::marker::PhantomData;
+
+use crate::traits::{Coefficient, NoStrategy, PauliWordTrait, Strategy};
+use crate::{config::Config, word::PauliWord};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Byte8<
+    const N: usize,
+    C: Coefficient,
+    St: Strategy = NoStrategy,
+    W: PauliWordTrait = PauliWord<[u8; N], fxhash::FxBuildHasher>,
+>(PhantomData<(C, St, W)>);
+
+impl<const N: usize, C: Coefficient, St: Strategy, W: PauliWordTrait> Config
+    for Byte8<N, C, St, W>
+{
+    type Storage = [u64; N];
+    type Coeff = C;
+    type BuildHasher = fxhash::FxBuildHasher;
+    type PauliWordType = W;
+    type Map = HashMap<W, C, Self::BuildHasher>;
+    type Strategy = St;
+}
+
+pub type Byte8F64<const N: usize, St = NoStrategy> = Byte8<N, f64, St>;

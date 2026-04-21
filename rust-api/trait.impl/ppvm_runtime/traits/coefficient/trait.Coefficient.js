9a2cb5eb55b89c60021472c79eb05cf9b72115da
Roger-luo <Roger-luo@users.noreply.github.com>
2026-04-21 15:12:31 +0000

    deploy: 8888f1f68bfa2159e269592ba227fb269d62142f

--- rust-api/trait.impl/ppvm_runtime/traits/coefficient/trait.Coefficient.js
+++ rust-api/trait.impl/ppvm_runtime/traits/coefficient/trait.Coefficient.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["ppvm_runtime",[]],["ppvm_sym",[["impl Coefficient for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]],["ppvm_tableau",[]]]);
+    const implementors = Object.fromEntries([["ppvm_runtime",[]],["ppvm_sym",[["impl <a class=\"trait\" href=\"ppvm_runtime/traits/coefficient/trait.Coefficient.html\" title=\"trait ppvm_runtime::traits::coefficient::Coefficient\">Coefficient</a> for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]],["ppvm_tableau",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[19,138,20]}
+//{"start":59,"fragment_lengths":[19,288,20]}

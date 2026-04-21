8e1adb4a29502a48bf043e7fbb0f88f5b5efa9e2
Roger-luo <Roger-luo@users.noreply.github.com>
2026-04-21 15:01:37 +0000

    deploy: ed907efc2b4e71a1c2c9a5de42e82a7c730ca798

--- rust-api/trait.impl/core/iter/traits/accum/trait.Sum.js
+++ rust-api/trait.impl/core/iter/traits/accum/trait.Sum.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["ppvm_sym",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a> for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]]]);
+    const implementors = Object.fromEntries([["ppvm_sym",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a> for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- rust-api/trait.impl/ppvm_runtime/traits/coefficient/trait.ComplexCoefficient.js
+++ rust-api/trait.impl/ppvm_runtime/traits/coefficient/trait.ComplexCoefficient.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["ppvm_runtime",[]],["ppvm_sym",[["impl <a class=\"trait\" href=\"ppvm_runtime/traits/coefficient/trait.ComplexCoefficient.html\" title=\"trait ppvm_runtime::traits::coefficient::ComplexCoefficient\">ComplexCoefficient</a> for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]],["ppvm_tableau",[]]]);
+    const implementors = Object.fromEntries([["ppvm_runtime",[]],["ppvm_sym",[["impl ComplexCoefficient for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]],["ppvm_tableau",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[19,309,20]}
+//{"start":59,"fragment_lengths":[19,145,20]}

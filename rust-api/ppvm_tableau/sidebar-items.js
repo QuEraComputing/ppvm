0d0aa7ea5312b81c5ff781445a67a90ffd387bb4
Roger-luo <Roger-luo@users.noreply.github.com>
2026-05-17 02:37:45 +0000

    deploy: dd2d9bfbf915cae45fdc66b2ec00fea95b613dbe

--- rust-api/ppvm_tableau/sidebar-items.js
+++ rust-api/ppvm_tableau/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["data","display","gates","measure","noise","prelude","sparsevec","stim","tableau_index"]};
+window.SIDEBAR_ITEMS = {"mod":["data","display","gates","measure","noise","prelude","sparsevec","stim","tableau_index","tableau_like"]};

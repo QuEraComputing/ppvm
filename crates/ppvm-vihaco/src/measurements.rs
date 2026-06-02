8ab3ebe052f66cf64c3c27a05996041cd06ba764
David Plankensteiner <dplankensteiner@quera.com>
2026-06-02 09:53:08 +0200

    Add comment to measurement outcome

--- crates/ppvm-vihaco/src/measurements.rs
+++ crates/ppvm-vihaco/src/measurements.rs
@@ -1,6 +1,12 @@
 use eyre::Result;
 use vihaco::{Effects, observe};
 
+/// Measurement results are represent as an integer enum
+/// 0: state |0>
+/// 1: state |1>
+/// 2: qubit has been lost prior to measurement
+/// In byte-code, this is represented as a u32 integer, which is simpler than
+/// e.g. two boolean values and matches semantics elsewhere
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum MeasurementOutcome {

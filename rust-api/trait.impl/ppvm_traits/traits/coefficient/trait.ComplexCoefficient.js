a59cdec1e99a9de2cd48bb509afc29db7a0f75d9
david-pl <david-pl@users.noreply.github.com>
2026-09-01 08:12:37 +0000

    deploy: 8f7d197bdb6b1c09d94fc0e78efa79da8021dd21

--- rust-api/trait.impl/ppvm_traits/traits/coefficient/trait.ComplexCoefficient.js
+++ rust-api/trait.impl/ppvm_traits/traits/coefficient/trait.ComplexCoefficient.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["ppvm_pauli_sum",[]],["ppvm_pauli_word",[]],["ppvm_sym",[["impl ComplexCoefficient for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]],["ppvm_tableau",[]],["ppvm_tableau_sum",[]],["ppvm_traits",[]]]);
+    const implementors = Object.fromEntries([["ppvm_pauli_sum",[]],["ppvm_pauli_word",[]],["ppvm_sym",[["impl <a class=\"trait\" href=\"ppvm_traits/traits/coefficient/trait.ComplexCoefficient.html\" title=\"trait ppvm_traits::traits::coefficient::ComplexCoefficient\">ComplexCoefficient</a> for <a class=\"struct\" href=\"ppvm_sym/struct.Term.html\" title=\"struct ppvm_sym::Term\">Term</a>",0]]],["ppvm_tableau",[]],["ppvm_tableau_sum",[]],["ppvm_traits",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[21,23,145,20,24,19]}
+//{"start":59,"fragment_lengths":[21,23,307,20,24,19]}
